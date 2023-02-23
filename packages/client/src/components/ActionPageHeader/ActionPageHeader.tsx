006a664316c03689342def3582eade094e5bf0ce
Shrey Tailor <39754888+shreytailor@users.noreply.github.com>
2023-02-23 21:12:32 +1300

    [Feature] Back button in `ActionPageHeader` can now navigate back.

--- packages/client/src/components/ActionPageHeader/ActionPageHeader.tsx
+++ packages/client/src/components/ActionPageHeader/ActionPageHeader.tsx
@@ -1,14 +1,25 @@
 import { PageTitle } from '../../typography';
-import { IoArrowBackCircleOutline } from 'react-icons/all';
 import classes from './ActionPageHeader.module.css';
+import { IoArrowBackCircleOutline } from 'react-icons/all';
+import { useNavigate } from 'react-router-dom';
+
 interface ActionPageHeaderProps {
   title: string;
 }
 
 export function ActionPageHeader({ title }: ActionPageHeaderProps) {
+  const navigate = useNavigate();
+
   return (
     <div className={classes.wrapper}>
-      <IoArrowBackCircleOutline size={32} color="hsla(29, 83%, 42%, 1)" />
+      <IoArrowBackCircleOutline
+        className={classes.backButton}
+        size={32}
+        color="hsla(29, 83%, 42%, 1)"
+        onClick={() => {
+          navigate(-1);
+        }}
+      />
       <PageTitle>{title}</PageTitle>
     </div>
   );
